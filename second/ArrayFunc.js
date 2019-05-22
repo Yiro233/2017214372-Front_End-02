fcbfd899146c2d85b1298695aaca0fa3bdbd0f2e
Yiro233 <45039297+Yiro233@users.noreply.github.com>
2019-05-22 15:57:59 +0800

    Update ArrayFunc.js

--- second/ArrayFunc.js
+++ second/ArrayFunc.js
@@ -1,29 +1,31 @@
-function func_1(array) {
-   let arr = new Array();
-   array.forEach(x => {
-       if(!arr.includes(x)){
-           arr.push(x);
-       }
-   });
-   return arr;
-}
-function func_2(array) {
-    let s = new Set();
-    let arr = new Array();
-    array.forEach(x => s.add(x));
-    s.forEach(x => arr.push(x));
-    return arr;
-}
-function func_3(array) {
-    let arr = new Array();
-    array.forEach(x => {
-        if(!arr.find((n) => n == x)){
-            arr.push(x);
+function deDupWithIncludes(array) {
+    let res = new Array();
+    array.forEach(src => {
+        if (!res.includes(src)) {
+            res.push(src);
         }
     });
-    return arr;
+    return res;
 }
-var array = [1,2,2,3,4,5,6,6,6,6];
-console.log(func_1(array));
-console.log(func_2(array));
-console.log(func_3(array));
+
+function deDupWithSet(array) {
+    let tranSet = new Set();
+    let res = new Array();
+    array.forEach(src => tranSet.add(src));
+    tranSet.forEach(src => res.push(src));
+    return res;
+}
+
+function deDupWithFind(array) {
+    let res = new Array();
+    array.forEach(des => {
+        if (!res.find((src) => src == des)) {
+            res.push(des);
+        }
+    });
+    return res;
+}
+var array = [1, 2, 2, 3, 4, 5, 6, 6, 6, 6];
+console.log(deDupWithIncludes(array));
+console.log(deDupWithSet(array));
+console.log(deDupWithFind(array));
